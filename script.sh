9c877902c91f96a87cc22858f7585c8cee36a51c
Syam <syamkumarputta1999@gmail.com>
2024-08-22 06:31:42 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+#author:
+#documentation:simple shell script for NareshIT frontdesk
+#date:
+#version:
+echo "NARESH IT"
+echo "Hai,this is Naresh IT Frontdesk.How can i help you"
+read 
+echo "May i know your name and your education qualification"
+read 
+echo "Thank you for the information, how do you know about this is institution"
+read
+echo "which course are you intrested in joining"
+read 
+echo "Absolutely, I'd be happy to provide more information. Our AWS DevOps course is designed to cover the essentials of AWS services along with DevOps practices."
+read 
+echo "You can enroll through our website or by visiting our office. I can send you the link to the online enrollment page if that’s more convenient for you."
+read
+echo "The next batch starts in two weeks. I’ll send you the enrollment link now. If you have any questions or need further assistance, don’t hesitate to ask."
+read
+echo "My pleasure. We look forward to having you in the AWS DevOps course!"
